d042336b32512017fd343a4f444b067e63fd4329
Noitcereon <tba@live.dk>
2024-12-09 10:14:47 +0100

    rest-mvc: Add newly created spring.io project w. Spring Web & Lombok

---
+++ spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.noitcereon.practice.spring6restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
