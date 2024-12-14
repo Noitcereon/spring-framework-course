381f57569f0b7095e5a1ebd419fa5741bd521b44
Noit-LinuxVM <tba@live.dk>
2024-12-14 15:33:16 +0100

    rest-mvc: Change BeerControllerTest to use Spring Test Slice

--- spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/controllers/BeerControllerTest.java
+++ spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,38 +1,31 @@
 package me.noitcereon.practice.spring6restmvc.controllers;
 
-import me.noitcereon.practice.spring6restmvc.models.Beer;
+import me.noitcereon.practice.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@WebMvcTest(BeerController.class) // A Spring "Test Slice" to minimize test setup. See also: https://docs.spring.io/spring-boot/appendix/test-auto-configuration/slices.html
 class BeerControllerTest {
 
     @Autowired
-    BeerController controller;
+    MockMvc mockMvc;
+
+    @MockitoBean
+    BeerService mockBeerService;
 
     @Test
-    void givenAnIdForSpecificBeer_WhenFetchingBeerById_ThenReturnThatBeer() {
-        String expectedBeerName = "Galaxy Cat";
-        Beer actual = controller.getBeerById(UUID.fromString("856477d4-ab9a-4d6f-a031-d44b8cb5c584"));
-
-        assertEquals(expectedBeerName, actual.getBeerName());
-    }
-    @Test
-    void givenNewBeer_WhenSavingNewBeer_ThenReturnCreatedResponse(){
-        Beer newBeer = Beer.builder()
-               .beerName("Test Beer")
-               .build();
-
-        ResponseEntity<Beer> actual = controller.createBeer(newBeer);
-
-        HttpStatus expectedStatus = HttpStatus.CREATED;
-        assertEquals(expectedStatus, actual.getStatusCode());
+    void fetchingBeerByIdShouldReturnOkResponse() throws Exception{
+        String endpoint = "/api/v1/beer/" + UUID.randomUUID();
+        mockMvc.perform(MockMvcRequestBuilders.get(endpoint)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
