8a6974aae0d4b11c3711255a0eca5e4f3be29e61
Noit-LinuxVM <tba@live.dk>
2024-12-16 15:22:51 +0100

    rest-mvc: Improve fetchBeerById test with BDDMockito.given

--- spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/controllers/BeerControllerTest.java
+++ spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,7 +1,9 @@
 package me.noitcereon.practice.spring6restmvc.controllers;
 
+import me.noitcereon.practice.spring6restmvc.models.Beer;
 import me.noitcereon.practice.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -11,6 +13,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @WebMvcTest(BeerController.class) // A Spring "Test Slice" to minimize test setup. See also: https://docs.spring.io/spring-boot/appendix/test-auto-configuration/slices.html
 class BeerControllerTest {
@@ -23,9 +27,14 @@ class BeerControllerTest {
 
     @Test
     void fetchingBeerByIdShouldReturnOkResponse() throws Exception{
+        Beer testBeer = Beer.builder().beerName("Test Beer").build();
         String endpoint = "/api/v1/beer/" + UUID.randomUUID();
+        BDDMockito.given(mockBeerService.getBeerById(any(UUID.class)))
+                .willReturn(testBeer);
+
         mockMvc.perform(MockMvcRequestBuilders.get(endpoint)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
     }
 }
