d3fec98eb67e1fcac234edc31d57b96e43fef120
Thomas 'Noitcereon' Andersen <thomas.andersen2@dxc.com>
2025-03-19 14:11:11 +0100

    Test(BeerController): Add create integration test

--- spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/controllers/BeerControllerIntegrationTest.java
+++ spring-6-rest-mvc/src/test/java/me/noitcereon/practice/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -1,11 +1,14 @@
 package me.noitcereon.practice.spring6restmvc.controllers;
 
 import jakarta.transaction.Transactional;
+import me.noitcereon.practice.spring6restmvc.entities.Beer;
 import me.noitcereon.practice.spring6restmvc.models.BeerDTO;
 import me.noitcereon.practice.spring6restmvc.repositories.BeerRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -14,6 +17,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -26,7 +31,7 @@ class BeerControllerIntegrationTest {
     private BeerRepo beerRepo;
 
     @Test
-    void testGetAllBeersSuccess(){
+    void testGetAllBeersSuccess() {
         List<BeerDTO> beerDtos = beerController.getBeerList();
 
         assertEquals(3, beerDtos.size());
@@ -35,7 +40,7 @@ class BeerControllerIntegrationTest {
     @Transactional
     @Rollback
     @Test
-    void givenNoBeerEntitiesInDatabase_WhenFetchingAllBeers_Then0BeersAreReturned(){
+    void givenNoBeerEntitiesInDatabase_WhenFetchingAllBeers_Then0BeersAreReturned() {
         beerRepo.deleteAll();
         List<BeerDTO> allBeers = beerController.getBeerList();
 
@@ -43,18 +48,37 @@ class BeerControllerIntegrationTest {
     }
 
     @Test
-    void givenUnknownId_WhenFetchingBeerById_ThenEmptyOptionalIsReturned(){
+    void givenUnknownId_WhenFetchingBeerById_ThenEmptyOptionalIsReturned() {
         UUID randomId = UUID.randomUUID();
         Optional<BeerDTO> beerDto = beerController.getBeerById(randomId);
         assertEquals(Optional.empty(), beerDto);
     }
 
     @Test
-    void givenExistingBeerId_WhenFetchingBeerById_ThenBeerIsFound(){
+    void givenExistingBeerId_WhenFetchingBeerById_ThenBeerIsFound() {
         UUID beerId = beerRepo.findAll().getFirst().getId();
 
         Optional<BeerDTO> beerDto = beerController.getBeerById(beerId);
 
         assertEquals(beerId, beerDto.orElseThrow().getId());
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testSaveNewBeerSuccess() {
+        BeerDTO beerDtoToCreate = BeerDTO.builder()
+                .beerName("New Beer")
+                .build();
+
+        ResponseEntity<BeerDTO> response = beerController.createBeer(beerDtoToCreate);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().getLocation());
+        BeerDTO createdBeerDto = response.getBody();
+        assertNotNull(createdBeerDto);
+        Optional<Beer> beerEntity = beerRepo.findById(createdBeerDto.getId());
+        assertTrue(beerEntity.isPresent());
+        assertEquals(beerDtoToCreate.getBeerName(), beerEntity.get().getBeerName());
     }
 }
